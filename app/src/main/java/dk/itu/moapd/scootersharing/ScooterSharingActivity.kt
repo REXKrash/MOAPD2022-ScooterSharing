3074d3e6381b358de6b032706927fe0da925a45a
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-02-01 12:05:07 +0100

    Minor refactor

--- app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.EditText
 import dk.itu.moapd.scootersharing.databinding.ActivityScooterSharingBinding
+import dk.itu.moapd.scootersharing.model.Scooter
+import dk.itu.moapd.scootersharing.model.getInfo
 
 class ScooterSharingActivity : AppCompatActivity() {
 
--- app/src/main/java/dk/itu/moapd/scootersharing/model/Scooter.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/model/Scooter.kt
@@ -1,4 +1,4 @@
-package dk.itu.moapd.scootersharing
+package dk.itu.moapd.scootersharing.model
 
 data class Scooter(
     var name: String,
