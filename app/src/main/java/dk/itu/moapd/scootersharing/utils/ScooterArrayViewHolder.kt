2186c22181456922cf4ecd99410f37c1ae6b0ec7
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-04-13 14:41:30 +0200

    Update list design

--- app/src/main/java/dk/itu/moapd/scootersharing/utils/ScooterArrayViewHolder.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/utils/ScooterArrayViewHolder.kt
@@ -1,9 +1,9 @@
 package dk.itu.moapd.scootersharing.utils
 
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
 import dk.itu.moapd.scootersharing.R
 
 class ScooterArrayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -11,5 +11,5 @@ class ScooterArrayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val nameTextView: TextView = itemView.findViewById(R.id.name_text)
     val whereTextView: TextView = itemView.findViewById(R.id.where_text)
     val timeTextView: TextView = itemView.findViewById(R.id.timestamp_text)
-    val rideLayout: LinearLayout = itemView.findViewById(R.id.ride_layout)
+    val rideLayout: MaterialCardView = itemView.findViewById(R.id.ride_layout)
 }
