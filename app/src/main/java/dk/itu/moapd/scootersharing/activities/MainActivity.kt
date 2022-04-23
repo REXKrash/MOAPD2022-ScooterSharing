65a87be7f13f1f202164737869c669d0a3127ee6
Thor Tudal Lauridsen <rexkrash@yahoo.dk>
2022-04-23 14:54:12 +0200

    Add new default scooters

--- app/src/main/java/dk/itu/moapd/scootersharing/activities/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+    private lateinit var scooterRepository: ScooterRepository
+    private lateinit var scooters: LiveData<List<Scooter>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +43,8 @@ class MainActivity : AppCompatActivity() {
             binding.bottomAppBar,
             navHostFragment!!.navController
         )
+        scooterRepository = ScooterRepository(application)
+        scooters = scooterRepository.getAll()
         seedDefaultBikes()
     }
 
@@ -49,43 +54,89 @@ class MainActivity : AppCompatActivity() {
 
         scooters.observe(this) {
             if (it.isEmpty()) {
-                lifecycleScope.launch {
 
-                    scooterRepository.insert(
-                        Scooter(
-                            1,
-                            "Scooter 1",
-                            "ITU",
-                            randomDate(),
-                            false,
-                            55.629526888870515,
-                            12.579146202544704
-                        )
-                    )
-                    scooterRepository.insert(
-                        Scooter(
-                            2,
-                            "Super Scooter 2",
-                            "Fields",
-                            randomDate(),
-                            false,
-                            55.6596,
-                            12.5930
-                        )
-                    )
-                    scooterRepository.insert(
-                        Scooter(
-                            3,
-                            "Rambo 3",
-                            "Kobenhavns Lufthavn",
-                            randomDate(),
-                            false,
-                            55.6586,
-                            12.5920
-                        )
-                    )
-                }
+                addScooter(
+                    1,
+                    "Fields",
+                    55.629526888870515,
+                    12.579146202544704
+                )
+                addScooter(
+                    2,
+                    "Bella Center",
+                    55.63787677915786,
+                    12.582772549132839
+                )
+                addScooter(
+                    3,
+                    "Ørestad Bibliotek",
+                    55.63124243188086,
+                    12.580912113189697
+                )
+                addScooter(
+                    4,
+                    "CF Møllers Allé",
+                    55.63565136426479,
+                    12.57451134536697
+                )
+                addScooter(
+                    5,
+                    "Martha Christensens vej",
+                    55.639351440372124,
+                    12.577665623168484
+                )
+                addScooter(
+                    6,
+                    "Royal Golf Center",
+                    55.63842177800499,
+                    12.570842083434597
+                )
+                addScooter(
+                    7,
+                    "Byparken",
+                    55.633661865368246,
+                    12.577601250152126
+                )
+                addScooter(
+                    8,
+                    "Irma",
+                    55.631542125301195,
+                    12.57502632949783
+                )
+                addScooter(
+                    9,
+                    "Amagerkollegiet",
+                    55.63379510234193,
+                    12.584403332213894
+                )
+                addScooter(
+                    10,
+                    "Amager",
+                    55.634957878517426,
+                    12.587171371917263
+                )
             }
+        }
+    }
+
+    private fun addScooter(
+        id: Int,
+        location: String,
+        latitude: Double,
+        longitude: Double,
+    ) {
+        lifecycleScope.launch {
+            scooterRepository.insert(
+                Scooter(
+                    id,
+                    "Scooter $id",
+                    location,
+                    randomDate(),
+                    false,
+                    latitude,
+                    longitude
+                )
+            )
         }
     }
 
